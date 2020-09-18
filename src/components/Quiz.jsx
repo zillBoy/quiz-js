aef13d687e5ec0dd7fd37dc0e07666d557aaad76
zillBoy <prousman741@gmail.com>
2020-09-18 13:02:27 +0500

    major changes added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,12 +10,13 @@ function Quiz() {
     const [difficulty, setDifficulty] = useState("Easy")
     
     const [totalCorrect, setTotalCorrect] = useState(0)
+    let quizCheck = []
 
     let API_KEY = "OSnosGEkHpodofWjteoZNuz2BngCwENXjglNjA8F"
     let url = `https://quizapi.io/api/v1/questions?apiKey=${API_KEY}&category=${category}&difficulty=Easy&limit=${limit}`
 
     const getQuizData = () => {
-        
+        console.log(url)
         axios(url)
         .then(res => {
             setQuizData(res.data)
@@ -26,46 +27,19 @@ function Quiz() {
     }
 
     const submitQuiz = () => {
-        let correctAnswers = quizData[0].correct_answers
-        let correctOption = ""
-        let userOption = ""
+        for (let i = 0; i < quizData.length; i++) {
+            let correctAnswer = Object.keys(quizData[i].correct_answers).filter(e => quizData[i].correct_answers[e] === "true")[0]
+            let userAnswer = quizCheck[i]
 
-        let a = document.getElementById("answer_a")
-        let b = document.getElementById("answer_b")
-        let c = document.getElementById("answer_c")
-        let d = document.getElementById("answer_d")
+            if (typeof(userAnswer) === 'undefined' || userAnswer.checked === false) {
+                alert(`Please answer the question#${i + 1}`)
+                return
+            }
 
-        if (a.checked) userOption = "a" 
-        else if (b.checked) userOption = "b"
-        else if (c.checked) userOption = "c"
-        else if (d.checked) userOption = "d"
-
-        if (userOption.length === 0) {
-            alert("Choose an option") 
-            return
-        }
-
-        for (let property in correctAnswers) {
-            if (correctAnswers[property]) {
-                correctOption = property
-                break
+            if(correctAnswer === userAnswer.answer+"_correct") {
+                setTotalCorrect(prevCount => prevCount + 1)
             }
         }
-
-        correctOption = correctOption.split("_")
-
-        if (correctOption[1] === userOption) {
-            setTotalCorrect(prevCount => prevCount + 1)
-        }
-
-        console.log(totalCorrect)
-
-        a.checked = false
-        b.checked = false
-        c.checked = false
-        if (c !== null) c.checked = false
-        if (d !== null) d.checked = false
-
         getQuizData()
     }
 
@@ -92,13 +66,13 @@ function Quiz() {
                     <div className="quiz-question" key={index}>
                         <h4>{index + 1}. {val.question}</h4>
                         <ol>
-                            <input type="checkbox" id="answer_a" /> <label>{val.answers.answer_a}</label>
+                            <input type="checkbox" id="answer_a" onChange={(e) => quizCheck[index] = {answer: e.target.id, checked: e.target.checked} }/> <label>{val.answers.answer_a}</label>
                             <br/>
-                            <input type="checkbox" id="answer_b"/> <label>{val.answers.answer_b}</label>
+                            <input type="checkbox" id="answer_b" onChange={(e) => quizCheck[index] = {answer: e.target.id, checked: e.target.checked} } /> <label>{val.answers.answer_b}</label>
                             <br/>
-                            {val.answers.answer_c !== null ? <><input type="checkbox" id="answer_c"/> <label>{val.answers.answer_c}</label></> : null}
+                            {val.answers.answer_c !== null ? <><input type="checkbox" id="answer_c" onChange={(e) => quizCheck[index] = {answer: e.target.id, checked: e.target.checked} } /> <label>{val.answers.answer_c}</label></> : null}
                             <br/>
-                            {val.answers.answer_d !== null ? <><input type="checkbox" id="answer_d"/> <label>{val.answers.answer_d}</label></> : null}
+                            {val.answers.answer_d !== null ? <><input type="checkbox" id="answer_d" onChange={(e) => quizCheck[index] = {answer: e.target.id, checked: e.target.checked} } /> <label>{val.answers.answer_d}</label></> : null}
                         </ol>
                     </div>
                 );
